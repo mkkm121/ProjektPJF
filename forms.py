68d4cec5224e81566ca4a868a43cd2d1df7fe59a
Maciej <maciek.kolodziej2000@o2.pl>
2021-12-29 16:03:36 +0100

    Changing login and register page. Adding messages.

--- forms.py
+++ forms.py
@@ -5,12 +5,12 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 class RegistrationForm(FlaskForm):
     username = StringField('Nazwa użytkownika', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
-    password = PasswordField('Haslo', validators=[DataRequired()])
-    confirm_password = PasswordField('Powtorz haslo', validators=[DataRequired(), EqualTo('password')])
+    password = PasswordField('Hasło', validators=[DataRequired()])
+    confirm_password = PasswordField('Powtórz hasło', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Zarejestruj!')
 
 class LoginForm(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
-    password = PasswordField('Haslo', validators=[DataRequired()])
+    password = PasswordField('Hasło', validators=[DataRequired()])
     remember = BooleanField('Zapamietaj mnie')
     submit = SubmitField('Zaloguj!')
